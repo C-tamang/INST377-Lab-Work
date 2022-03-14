cc275f56470b924deb8d7b76701b3c33e7d735b3
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-03-13 21:05:25 -0400

    fixed eslint error

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -9,14 +9,27 @@ function getRandomIntInclusive(min, max) {
 // Data Handler
 function dataHandler(dataArray) {
   console.log('fired dataHandler')
-  console.table(dataArray)
+  //console.table(dataArray)
   const range = [...Array(15).keys()]
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1)
     return dataArray[restNum]
   })
-  console.log(listItems)
+  //console.log(listItems)
   return listItems
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator')
+  console.log(collection)
+  const targetList = document.querySelector('.resto-list')
+  targetList.innerHTML = ''
+  collection.forEach((item) => {
+    const {name} = item
+    const displayName = name.toLowerCase()
+    const injectThisItem = `<li>${displayName}</li>`
+    targetList.innerHTML += injectThisItem
+  });
 }
 
 // Main function
@@ -36,22 +49,10 @@ async function mainEvent() { // the async keyword means we can make API requests
       submitEvent.preventDefault()
       console.log('form submission')
       const restoArray = dataHandler(arrayFromJson.data)
+      createHtmlList(restoArray)
     })
 
   }
-
-  
-  /*
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
-  */
 
 
 }
