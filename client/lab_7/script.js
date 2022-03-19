ef32bc293183802162c6af9d54b5fa730c3c43cd
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-03-19 15:17:57 -0400

    copied lab 6 into lab 7

---
+++ client/lab_7/script.js
@@ -0,0 +1,76 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  );
+}
+
+// Data Handler
+function dataHandler(dataArray) {
+  // console.log('fired dataHandler');
+  // console.table(dataArray)
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+    // console.log(listItems)
+  return listItems;
+}
+
+function createHtmlList(collection) {
+  // console.log('fired HTML creator');
+  // console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  // console.log(targetList)
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+// Main function
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('.submit_button');
+
+  const resto = document.querySelector('#resto_name');
+  const zipcode = document.querySelector('#zipcode');
+  submit.style.display = 'none';
+
+  // const results = await fetch('/api/foodServicesPG');
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const arrayFromJson = await results.json();
+  // console.log(arrayFromJson);
+
+  if (arrayFromJson.length > 0) {
+    submit.style.display = 'block';
+
+    const currentArray = [];
+    resto.addEventListener('input', async (event) => {
+      if (currentArray === undefined) { return; }
+      console.log(event.target.value);
+      const matchResto = currentArray.filter((item) => {
+        console.log(item)
+        console.log(item.name)
+        item.name.includes(event.target.value)
+      });
+      // connsole.log(matchResto);
+    })
+
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      // console.log('form submission');
+      const restoArray = dataHandler(arrayFromJson);
+      createHtmlList(restoArray);
+    });
+  }
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
