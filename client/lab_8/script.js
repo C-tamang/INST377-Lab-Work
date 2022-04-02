f0ad517a0d906b800081ef49ebddc1c52c1034cb
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-02 18:22:40 -0400

    change arguments

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -33,8 +33,8 @@ function createHtmlList(collection) {
   });
 }
 
-function initMap() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
+function initMap(targetId) {
+  const map = L.map(targetId).setView([51.505, -0.09], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -54,7 +54,7 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const resto = document.querySelector('#resto_name');
   const zipcode = document.querySelector('#zipcode');
-  const map = initMap();
+  const map = initMap('map');
   submit.style.display = 'none';
 
   // const results = await fetch('/api/foodServicesPG');
